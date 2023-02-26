7a6f514aad69c89f63e2f493ce68f845ef25e993
wabibaah <wabisernproject@gmail.com>
2023-02-26 11:26:19 +0000

    get all ads created by user to the dashboard, add more pagination

---
+++ src/components/cards/UserAdCard.js
@@ -0,0 +1,34 @@
+import { Badge } from "antd";
+import { Link } from "react-router-dom";
+import AdFeatures from "./AdFeatures";
+import { formatNumber } from "../../helpers/ad";
+
+function UserAdCard({ ad }) {
+  return (
+    <div className="col-lg-4 p-4 gx-4 gy-4" key={ad._id}>
+      <Link to={`/user/ad/${ad.slug}`}>
+        <Badge.Ribbon
+          text={`${ad?.type} for ${ad?.action}`}
+          color={`${ad?.action === "Sell" ? "blue" : "red"}`}
+        >
+          <div className="card hoverable shadow">
+            <img
+              src={ad?.photos?.[0].Location}
+              alt={`${ad?.type}-${ad?.address}-${ad?.address}-${ad?.action}-${ad?.price}`}
+              style={{ height: "250px", objectFit: "cover" }}
+            />
+            <div className="card-body">
+              <h3>GH${formatNumber(ad?.price)}</h3>
+              {/* you can show the address only too or select some if your app is finally working with aws and google */}
+              <p className="card-text">{`${ad?.type}-${ad?.address}-${ad?.address}-${ad?.action}-${ad?.price}`}</p>
+
+              <AdFeatures ad={ad} />
+            </div>
+          </div>
+        </Badge.Ribbon>
+      </Link>
+    </div>
+  );
+}
+
+export default UserAdCard;
--- src/pages/user/Dashboard.js
+++ src/pages/user/Dashboard.js
@@ -1,11 +1,96 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 import { useAuth } from "../../context/auth";
 import Sidebar from "../../components/nav/Sidebar";
+import UserAdCard from "../../components/cards/UserAdCard";
 
 function Dashboard() {
+  const [auth, setAuth] = useAuth();
+
+  const [ads, setAds] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  const seller = auth.user?.role?.includes("Seller");
+
+  useEffect(() => {
+    fetchAds();
+  }, [auth.token !== ""]);
+
+  useEffect(() => {
+    if (page === 1) return;
+    loadMore();
+  }, [page]);
+
+  const fetchAds = async () => {
+    try {
+      const { data } = await axios.get(`/ad/get-user-ads/${page}`);
+      setAds(data.ads);
+      setTotal(data.total);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const loadMore = async () => {
+    try {
+      setLoading(true);
+      // i was thinking we should give this one to the body but he intentionally gave it to the params as the backend is receiving it ,,,, sharp sharp sharp
+      const { data } = await axios.get(`/ad/get-user-ads/${page}`);
+      setAds([...ads, ...data.ads]);
+      setTotal(data.total);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+    }
+  };
+
   return (
     <div>
-      <Sidebar />
       <h1>Dashboard</h1>
+      <Sidebar />
+      {!seller ? (
+        <div
+          className="d-flex justify-content-center align-items-center vh-100"
+          style={{ marginTop: "-20%" }}
+        >
+          <h2>Hey, {auth.user.name ? auth.user.name : auth.user.username}, welcome to Wabi app</h2>
+        </div>
+      ) : (
+        <div className="container ">
+          <div className="row">
+            <div className="col-lg-8 offset-lg-2 my-4">
+              <h3 className="text-center">Total {total} found</h3>
+            </div>
+          </div>
+          <div className="row">
+            {ads?.map((ad) => (
+              <UserAdCard ad={ad} />
+            ))}
+          </div>
+          {ads?.length < total ? (
+            <div className="row">
+              <div className="col text-center my-4">
+                <button
+                  className="btn btn-warning"
+                  disabled={loading}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setPage(page + 1);
+                  }}
+                >
+                  {loading ? "Processing" : `${ads?.length} / ${total} Load more`}
+                </button>
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
+      )}
     </div>
   );
 }
